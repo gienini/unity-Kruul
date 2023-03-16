8aef0229b4e32208c94e3ee9a1d869f7cc09973b
Francisco Javier Gienini <fgienini@seidor.es>
2023-03-16 19:02:28 +0100

    ejecutable

--- Assets/Tiles/CoordinateBrush/CoordinateBrush.cs
+++ Assets/Tiles/CoordinateBrush/CoordinateBrush.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Tilemaps;
@@ -88,3 +89,4 @@ namespace UnityEditor
         }
     }
 }
+#endif
