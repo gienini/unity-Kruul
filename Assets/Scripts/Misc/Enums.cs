aff756a803f2cfec30ed705fd0aeb54e75a51d73
Francisco Javier Gienini <fgienini@seidor.es>
2023-03-21 12:53:07 +0100

    SO sonido

--- Assets/Scripts/Misc/Enums.cs
+++ Assets/Scripts/Misc/Enums.cs
@@ -21,3 +21,22 @@ public enum NombresEscena
     Escena_2,
     none
 }
+/// <summary>
+/// Relacion de eventos de sonido
+/// </summary>
+public enum SoundName
+{
+    none = 0,
+    menuPrincipalMusica = 10,
+    menuPrincipalBoton = 20,
+    menuSettingsMusica = 30,
+    partidaSeleccionarAccionCarta = 50,
+    partidaPonerCarta = 60,
+    partidaSeleccionarAccionFicha = 70,
+    partidaPonerFicha = 80,
+    partidaEventoSeleccionAccion = 90,
+    partidaInicio = 95,
+    partidaFinalFase1 = 100,
+    partidaFinalPartidaGanador = 110,
+    partidaFinalPartidaEmpate = 120
+}
---
+++ Assets/Scripts/SO_Scripts/SO_Sonidos.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "so_PropiedadesCasilla", menuName = "Scriptable Objects/Sonidos")]
+
+public class SO_Sonidos : ScriptableObject
+{
+    [SerializeField]
+    public List<SoundItem> soundDetails;
+}
